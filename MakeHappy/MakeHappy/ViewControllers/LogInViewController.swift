c81d59ddca1cc5a8d6bb41f4bebdacadc364c683
Juliana Hong <julianahhong@gmail.com>
2015-07-30 16:09:40 -0400

    Added Log In View Controller

---
+++ MakeHappy/MakeHappy/ViewControllers/LogInViewController.swift
@@ -0,0 +1,24 @@
+//
+//  LogInViewController.swift
+//  MakeHappy
+//
+//  Created by Juliana Hong on 7/30/15.
+//  Copyright (c) 2015 Juliana Hong. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class LogInViewController: UIViewController {
+   
+    @IBOutlet var usernameTextField: UITextField!
+    
+    @IBOutlet var passwordTextField: UITextField!
+    
+    @IBOutlet var signInButton: UIButton!
+    
+    @IBOutlet var signUpButton: UIButton!
+    
+    @IBOutlet var signUpLaterButton: UIButton!
+    
+}
